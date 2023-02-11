da322b677e2d9c72053e09e9982322883e2823cf
Stu Wares <stuwares@outlook.com>
2023-02-11 14:52:15 +0000

    started scriptin

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,5 @@
-let username = "";
+let username = "guest";
+let currentDate = new Date();
+
+document.getElementById('welcome').textContent = currentDate + ' ba2h console';
+document.getElementById('user-intro').textContent = 'You are currently logged in as: ' + username;
