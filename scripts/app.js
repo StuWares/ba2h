deb11e48c135496ae2eac5b13ea61dbd3c7b4735
Stu Wares <stuwares@outlook.com>
2023-02-12 20:32:42 +0000

    started dev branch

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,31 @@
 let username = "guest";
 let currentDate = new Date();
+const cmdSubmit = document.getElementById('cmd-input');
+let commands = [];
+let consoleHistory = [];
+let inputHistory = [];
 
 document.getElementById('welcome').textContent = currentDate + ' ba2h console';
 document.getElementById('user-intro').textContent = 'You are currently logged in as: ' + username;
+
+let updateConsole = () => {
+    let separator = '<br>';
+    cmdSubmit.value = '';
+    document.getElementById('console-history').innerHTML = consoleHistory.join(separator);
+};
+
+
+cmdSubmit.addEventListener('keyup', event => {
+    if (event.key === 'Enter') {
+        console.log('enter key pressed')
+        let newCommand = cmdSubmit.value;
+        consoleHistory.push(newCommand);
+        updateConsole();
+    }
+})
+
+
+
+
+
+
